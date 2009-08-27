9dc43cc8451353913cd1783e69627fd7dde228b7
John Prince <jtprince@gmail.com>
2009-08-26 18:12:05 -0600

    progress

--- lib/ms/error_rate.rb
+++ lib/ms/error_rate.rb
@@ -2,5 +2,11 @@
 
 module Ms
   module ErrorRate
+    module SampleBias
+      # peptides must respond to transmembrane and return a value of 0 to 1.
+      # 0 means all its associated proteins do not have tm segments.  1 means
+      # all of them do.  Fractions OK.
+      def transmembrane(peptides, 
+    end
   end
 end
--- spec/ms/error_rate_spec.rb
+++ spec/ms/error_rate_spec.rb
@@ -3,15 +3,25 @@ require File.expand_path( File.dirname(__FILE__) + '/../tap_spec_helper' )
 require 'ms/error_rate'
 require 'ostruct'
 
+
+
 class ErrorRateBase < MiniTest::Spec
 
   before(:each) do
   end
 
   it 'calculates bayesian probabilities' do
-    # p(a|b) = p(b|a)p(a) / p(b)
-    p_b_given_a = 0.6 # prob of sample info given its correct
+    # C = is a correct ID
+    # T = transmembrane content
+    # Y = cysteine content
+    # A = abundance
+    # p(C|T,Y,A) = p(T|C)p(Y|C)p(A|C)p(C) / p(T)p(Y)p(A)
     peps.map do |pep|
+      # what is the probability of that un-transmembraneyness being correct?
+      # what is the probability of that un-cysteineness being correct?
+      # what is the probability of that high abundanceness being correct?
+      pep.bayes_probs.reduce(prob_being_correct do |prob| 
+      end
       p_correct = pep.prior_prob_correct
       pep.not_transmembrane? * pep.not_cysteine? * pep.not_low_abundance?
     end
