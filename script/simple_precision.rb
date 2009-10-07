983ba6cdfe74667b3f4c315a7becf2b8e070b07f
John Prince <jtprince@gmail.com>
2009-10-07 17:36:57 -0600

    can group by charge, implement monotonic precision now

--- script/simple_precision.rb
+++ script/simple_precision.rb
@@ -6,12 +6,13 @@ require 'ms/error_rate/decoy'
 require 'ms/mascot/dat'
 
 
-# returns an array of target hits containing:
-# [filename, sequence, charge, mowse, precision]
-# sorted by mowse score
-def calc_precision(target_files, decoy_files)
+# returns a hash of arrays of hits (each being: [filename, sequence, charge,
+# mowse, precision]) indexed by the charge state
+# if opts[:z_together], returns a single array
+def mascot_precision(target_files, decoy_files, opts={})
+  opts = {:z_together => false}.merge opts
 
-  all_hits = []
+  all_hits_by_charge = Hash.new {|h,k| h[k] = [] }
   (target_hash, decoy_hash) = [target_files, decoy_files].map do |files|
     hash = {}
 
@@ -21,30 +22,76 @@ def calc_precision(target_files, decoy_files)
       Ms::Mascot::Dat.open(file) do |dat|
         dat.each_peptide_hit(:yield_nil => false, :with_query => true) do |hit,query|
           hash[hit] = [filename, hit.sequence, query.data['charge'], hit.score] 
-          all_hits << hit
+          all_hits_by_charge[query.data['charge']] << hit
         end
       end
     end
-    hash
+    hash  # <-- need this for the map
   end
 
-  sorted_by_highest_mowse = all_hits.sort_by {|v| v.score }.reverse
+  # Proc.new doesn't do arity checking
+  hits_with_precision = Proc.new do |hits|
+    target_hits_as_arrays_sorted = []
+    all_sorted_by_mowse = hits.sort_by{|hit| -(hit.score)}
+    (target_hits, precisions) = precision(all_sorted_by_mowse, target_hash)
+    target_hits.zip(precisions) do |hit, precision|
+      target_hits_as_arrays_sorted.push( target_hash[hit] << precision )
+    end
+    target_hits_as_arrays_sorted
+  end
 
-  num_target = 0
-  num_decoy = 0
+  if opts[:z_together]
+    all_hits = []
+    all_hits_by_charge.each do |charge, hits|
+      all_hits.push(*hits)
+    end
+    hits_with_precision.call(all_hits)
+  else
+    all_hits = []
+    all_hits_by_charge.each do |charge,hits|
+      all_hits.push(*(hits_with_precision.call(hits)))
+    end
+    all_hits.sort_by {|v| -(v.last) }
+  end
+end
 
-  target_hits = sorted_by_highest_mowse.map do |hit|
-    if target_hash.key?(hit) 
+
+
+# returns [target_hits, precisions] (parallel arrays sorted from best hit to
+# worst hit).  expects an array-like object of hits sorted from best to worst
+# hit with decoys interspersed and a target_setlike object that responds to
+# :include? for the hit object assumes the hit is a decoy if not found in the
+# target set!  if monotonic is true, then the precision is guaranteed to be
+# the same or increase as score increases, otherwise it is possible to have a
+# lower scoring hit with a higher precision since precision is calculated from
+# a minimum threshold looking up in score.
+def precision(mixed_hits_sorted, target_setlike, opts={:monotonic => true})
+  num_target = 0 ; num_decoy = 0
+  monotonic = opts[:monotonic]
+  target_hits = []
+  precisions = []
+  mixed_hits_sorted.each do |hit|
+    if target_setlike.include?(hit) 
       num_target += 1
       precision = Ms::ErrorRate::Decoy.precision(num_target, num_decoy)
-      target_hash[hit] << precision
-    elsif decoy_hash.key?(hit)
-      num_decoy += 1
-      nil
+      target_hits << hit
+      precisions << precision
     else
-      raise "hit must be target or decoy I reckon"
+      num_decoy += 1
     end
-  end.compact
+  end
+  if opts[:monotonic]
+    max_precision = precisions.last 
+    precisions = precisions.reverse.map do |val| # from worst to best score
+      if max_precision > val 
+        max_precision
+      else
+        max_precision = val
+        val
+      end
+    end.reverse
+  end
+  [target_hits, precisions]
 end
 
 headers = %w(filename aaseq charge mowse precision)
@@ -69,8 +116,9 @@ opts = OptionParser.new do |op|
   op.separator "    data: (an array with the data values)"
   op.separator "headers: #{headers.join(', ')}"
   op.separator ""
+  op.on("--z-together", "combines all charge states for precision calc") {|v| opt[:z_together] = v }
   op.on("-o", "--outfile <name>", "write to specified file") {|v| opt[:outfile] = v }
-  op.on("-a", "--all-together", "process all forwards together and all decoys together", "will output to opt[:outfile] unless -o given") {|v| opt[:all_together] = v }
+  op.on("-g", "--group-together", "process all forwards together and all decoys together", "will output to opt[:outfile] unless -o given") {|v| opt[:group_together] = v }
   op.on("-f", "--find-decoy [ext]", "finds the decoy file, default <file>#{DEF_EXT}.dat", "obviating the need to specify it on the commandline") do |v| 
     if v.is_a? String
       opt[:find_decoy] = v
@@ -106,21 +154,24 @@ else
   end
 end
 
-filenames = { 'target' => target_files, 'decoy' => decoy_files }
 
-if opt[:all_together]
-  target_hits = calc_precision(target_files, decoy_files)
-  outfile = opt[:outfile]
+print_out = Proc.new do |outfile, filenames, headers, target_hits|
   File.open(outfile, 'w') do |out|
     out.print( {'headers' => headers, 'filenames' => filenames, 'data' => target_hits }.to_yaml )
   end
+end
+
+if opt[:group_together]
+  filenames = { 'target' => target_files, 'decoy' => decoy_files }
+  target_hits= mascot_precision(target_files, decoy_files, opt)
+  outfile = opt[:outfile]
+  print_out.call(outfile, filenames, headers, target_hits)
 else
   target_files.zip(decoy_files) do |target_file, decoy_file|
-    target_hits = calc_precision([target_file], [decoy_file])
+    filenames = { 'target' => [target_file], 'decoy' => [decoy_file] }
+    target_hits = mascot_precision([target_file], [decoy_file], opt)
     base = target_file.chomp(File.extname(target_file))
     outfile = base + '.' + NORMAL_EXT
-    File.open(outfile, 'w') do |out|
-      out.print( {'headers' => headers, 'filenames' => filenames, 'data' => target_hits }.to_yaml )
-    end
+    print_out.call(outfile, filenames, headers, target_hits)
   end
 end
